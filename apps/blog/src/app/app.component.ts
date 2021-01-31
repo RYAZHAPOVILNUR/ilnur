a3332f2a24b588e0e5047c178f0ec2bac8c53f8b
ilnur_ryazhapov <ilnur_abu_yahya@mail.ru>
2021-01-31 10:40:36 +0200

    simple todo started

--- apps/blog/src/app/app.component.ts
+++ apps/blog/src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+interface Todo {
+  id: number;
+  title: string;
+  checked: boolean
+}
+
 @Component({
   selector: 'ilnur-root',
   templateUrl: './app.component.html',
@@ -7,4 +13,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'blog';
+
+  todos: Todo[] = []
+  todoName: string
+
+  createTodo() {
+    const newTodo: Todo = {
+      id: new Date().getTime(),
+      title: this.todoName,
+      checked: false
+    }
+
+    this.todos.push(newTodo)
+  }
 }
--- apps/blog/src/app/app.module.ts
+++ apps/blog/src/app/app.module.ts
@@ -6,10 +6,22 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MatIconModule, MatToolbarModule, MatButtonModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
