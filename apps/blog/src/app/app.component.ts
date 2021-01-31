e009ef50673245819134264dc8a383f875860197
vadim <vadim.zubairov1993@gmail.com>
2021-01-31 19:26:57 +0500

    'first'

--- apps/blog/src/app/app.component.ts
+++ apps/blog/src/app/app.component.ts
@@ -24,6 +24,21 @@ export class AppComponent {
       checked: false
     }
 
-    this.todos.push(newTodo)
+    if(this.todoName.trim()) {
+      this.todos.push(newTodo)
+    }
+    this.todoName = ''
+  }
+
+  removeTodo(id: number) {
+    this.todos = this.todos.filter(todo => todo.id != id)
+  }
+
+  onChange(id: number) {
+    this.todos.map(todo => {
+      if(todo.id == id) {
+        todo.checked = !todo.checked
+      }
+    })
   }
 }
